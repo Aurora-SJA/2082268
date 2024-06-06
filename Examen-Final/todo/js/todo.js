e9be16873afc6a32ba7331a9438e17a8402d1bea
Aurora-SJA <aurorasolangejoaquimadao@gmail.com>
2024-06-06 08:56:39 -0300

    exam

---
+++ Examen-Final/todo/js/todo.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+const taskInput = document.getElementById("taskInput");
+const addButton = document.getElementById("addButton");
+const deleteButton = document.getElementById("deleteButton");
+const taskList = document.getElementById("taskList");
+
+taskInput.focus();
+
+});
